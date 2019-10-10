0e8d2d8c7aa7ae5205300dd861b14a6cb19a4cb3
HoangVietSE <viethoangtien@gmail.com>
2019-10-11 00:02:00 +0700

    Static header retrofit

--- base/src/main/java/com/soict/hoangviet/baseproject/data/network/api/BaseRetrofit.kt
+++ base/src/main/java/com/soict/hoangviet/baseproject/data/network/api/BaseRetrofit.kt
@@ -4,16 +4,14 @@ import com.google.gson.Gson
 import com.soict.hoangviet.baseproject.BuildConfig
 import com.soict.hoangviet.baseproject.data.network.ApiConstant
 import com.soict.hoangviet.baseproject.data.network.ApiService
-import okhttp3.MultipartBody
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
-open class BaseRetrofit{
+open class BaseRetrofit {
     companion object {
         private lateinit var apiService: ApiService
         private lateinit var retrofit: Retrofit
@@ -25,6 +23,16 @@ open class BaseRetrofit{
             mOkHttpClientBuilder.readTimeout(ApiConstant.Timeout.READ, TimeUnit.SECONDS)
             mOkHttpClientBuilder.writeTimeout(ApiConstant.Timeout.WRITE, TimeUnit.SECONDS)
             mOkHttpClientBuilder.callTimeout(ApiConstant.Timeout.CALL, TimeUnit.SECONDS)
+            mOkHttpClientBuilder.addInterceptor(object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val original = chain.request()
+                    val request = original.newBuilder()
+                        .addHeader("Content-Type", "application/json")
+                        .method(original.method(), original.body())
+                        .build()
+                    return chain.proceed(request)
+                }
+            })
             if (BuildConfig.DEBUG) {
                 mOkHttpClientBuilder?.let {
                     if (!it.interceptors().contains(logging)) {
