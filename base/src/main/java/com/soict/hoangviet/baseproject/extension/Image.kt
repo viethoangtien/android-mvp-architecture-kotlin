34065b16a778c7863f9892857b1c27fb907d15a5
viethoangtienbeetech <viet.ht@beetechsoft.com>
2020-05-12 13:54:14 +0700

    Listener for load image

--- base/src/main/java/com/soict/hoangviet/baseproject/extension/Image.kt
+++ base/src/main/java/com/soict/hoangviet/baseproject/extension/Image.kt
@@ -1,9 +1,14 @@
 package com.soict.hoangviet.baseproject.extension
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.soict.hoangviet.baseproject.module.GlideApp
 
 /**
@@ -24,5 +29,39 @@ inline fun <reified T : Any> ImageView.loadImage(
                 .into(this)
         }
     }
-
 }
+
+fun ImageView.loadImageListener(
+    context: Context,
+    image: Any?,
+    onSuccessListener: (() -> Unit)? = null,
+    onFailureListener: (() -> Unit)? = null
+) {
+    GlideApp.with(context)
+        .load(image)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                onFailureListener?.invoke()
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                onSuccessListener?.invoke()
+                return false
+            }
+
+        })
+        .into(this)
+}
+
